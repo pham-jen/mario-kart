76d441354227a911fc38d3a35415a1ba514c783e
jpham35 <49501108+jpham35@users.noreply.github.com>
2020-10-12 21:22:22 -0500

    Initial Commit

---
+++ mario-kart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mario-kart
+//
+//  Created by Jennifer Pham on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
